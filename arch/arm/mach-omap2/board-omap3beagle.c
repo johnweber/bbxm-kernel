7513e486ee5cdc89bc2b575ebd9b01dbf5c0198d
John Weber <rjohnweber@gmail.com>
2011-09-14 13:31:01 -0500

    Added missing bracket {

--- arch/arm/mach-omap2/board-omap3beagle.c
+++ arch/arm/mach-omap2/board-omap3beagle.c
@@ -730,7 +730,8 @@ static struct i2c_board_info __initdata beagle_i2c2_boardinfo[] = {
 		I2C_BOARD_INFO("tlc59108", 0x40),
 	},
 #if defined(CONFIG_RTC_DRV_DS1307) || \
-	defined(CONFIG_RTC_DRV_DS1307_MODULE)
+	defined(CONFIG_RTC_DRV_DS1307_MODULE) 
+	{
 		I2C_BOARD_INFO("ds1307", 0x68),
 	},
 #endif
